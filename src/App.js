46500ffd15889cedb4e45d82b5254ee09d1d1f8e
Jeff Shelley <hea45992@adobe.com>
2022-12-01 21:12:42 -0600

    more

--- src/App.js
+++ src/App.js
@@ -10,46 +10,65 @@ Picker,
 Item,
 Divider,
 Slider,
+Switch,
 Text,
-Content,
+Button,
   Provider} from '@adobe/react-spectrum';
 
 
+const fnConfig = {
+  "sin": {fn:Math.sin, layout:{xaxis:{range:[-6.28,6.28]},yaxis:{range:[-6,6]}}},
+  "cos": {fn:Math.cos, layout:{xaxis:{range:[-6.28,6.28]},yaxis:{range:[-6,6]}}},
+  "tan": {fn:Math.tan, layout:{xaxis:{range:[-6.28,6.28]},yaxis:{range:[-12,12]}}},
+  "csc": {fn:x => 1/Math.sin(x), layout:{xaxis:{range:[-6.28,6.28]},yaxis:{range:[-12,12]}}},
+  "sec": {fn:x => 1/Math.cos(x), layout:{xaxis:{range:[-6.28,6.28]},yaxis:{range:[-12,12]}}},
+  "cot": {fn:x => 1/Math.tan(x), layout:{xaxis:{range:[-6.28,6.28]},yaxis:{range:[-12,12]}}}
+}
+
 
 function Controls({fn, setFn, controls, setControls}) {
-
-
   const setValue = dim => v => {
     const newData = {...controls}
     newData[dim] = v;
     setControls(newData)
   }
 
+  const resetAll = () => {
+    setControls({a:1,b:1,c:0,d:0,refOnly:true})
+  }
 
   return (
     <Flex direction="column" gap="size-100" alignItems="center">
       <Picker label="Select Function" onSelectionChange={setFn} defaultSelectedKey={fn}>
         <Item key="sin">Sine</Item>
         <Item key="cos">Cosine</Item>
+        <Item key="tan">Tangent</Item>
+        <Item key="csc">Cosecant</Item>
+        <Item key="sec">Secant</Item>
+        <Item key="cot">Cotangent</Item>
       </Picker>
+      <Switch value={controls.refOnly} onChange={setValue("refOnly")} defaultSelected>Use Reference Points Only</Switch>
       <Divider />
         <Text>y={controls.a}*{fn}({controls.b}*x+{controls.c})+{controls.d}</Text>
-        <Slider label="A" minValue={-2} maxValue={2} step={.1} defaultValue={1} onChange={setValue("a")}/>
-        <Slider label="B" minValue={-2} maxValue={2} step={.1} defaultValue={1} onChange={setValue("b")}/>
-        <Slider label="C" minValue={-2} maxValue={2} step={.1} defaultValue={0} onChange={setValue("c")}/>
-        <Slider label="D" minValue={-2} maxValue={2} step={.1} defaultValue={0} onChange={setValue("d")}/>
+        <Slider label="A" value={controls.a} minValue={-2} maxValue={2} step={.1} defaultValue={1} onChange={setValue("a")}/>
+        <Slider label="B" value={controls.b} minValue={-2} maxValue={2} step={.1} defaultValue={1} onChange={setValue("b")}/>
+        <Slider label="C" value={controls.c} minValue={-2} maxValue={2} step={.1} defaultValue={0} onChange={setValue("c")}/>
+        <Slider label="D" value={controls.d} minValue={-2} maxValue={2} step={.1} defaultValue={0} onChange={setValue("d")}/>
+        <Divider />
+        <Button onClick={resetAll}>Reset</Button>
     </Flex>
   )
 }
 
 
 
-function Chart({data}) {
+function Chart({data, fn}) {
   return (
     <Plot
       data={[
         {type: 'line', x:data.x, y:data.y,text:data.text},
       ]}
+      layout={fnConfig[fn].layout}
     />
   )
 }
@@ -109,18 +128,13 @@ const refPoints = [
 ]
 
 
-const fnConfig = {
-  "sin": {fn:Math.sin},
-  "cos": {fn:Math.cos}
-}
-
 
 function App() {
 
   const [data,setData] = useState({})
 
   const [fn, setFn]=useState("sin")
-  const [controls, setControls]=useState({a:1,b:1,c:0,d:0})
+  const [controls, setControls]=useState({a:1,b:1,c:0,d:0,refOnly:true})
   const [initialized, setInitialized]=useState(false)
 
 
@@ -131,17 +145,36 @@ function App() {
 
       const mapped = x => controls.a * fnConfig[fn].fn(controls.b * x + controls.c) + controls.d
 
-      const plotX = refPoints.map(p => p.x)
-      const plotY = plotX.map(mapped)
-      const plotText = refPoints.map(p => p.lr)
+      let newData = null;
+      if(controls.refOnly) {
+        const plotX = refPoints.map(p => p.x)
+        const plotY = plotX.map(mapped)
+        const plotText = refPoints.map(p => `Ref Point: ${p.lr}`)
 
-      const newData = {x:plotX,y:plotY,text:plotText}
+        newData = {x:plotX,y:plotY,text:plotText}
+      }
+      else {
+
+        const lb = fnConfig[fn].layout.xaxis.range[0]
+        const ub = fnConfig[fn].layout.xaxis.range[1]
+
+        const plotX = []
+        const plotY = []
+
+        for(let x = lb; x < ub; x+=.1) {
+          plotX.push(x)
+          plotY.push(mapped(x))
+        }
+
+        newData = {x:plotX,y:plotY}
+      }
+
       setData(newData)
       if(!initialized) {
         setInitialized(true)
       }
     
-  }, [fn, controls])
+  }, [fn, controls,initialized])
 
 
   return (
@@ -152,7 +185,7 @@ function App() {
           </View>
           {initialized &&
           <View flex>
-            <Chart data={data}/>
+            <Chart data={data} fn={fn}/>
           </View>}
         </Flex>
     </Provider>
